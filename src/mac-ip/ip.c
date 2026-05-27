36ef3cd931a7dd4da43347714e3de12fe40809cc
Eliott Wanner <eliott.wanner@etu.unistra.fr>
2026-05-27 22:36:13 +0200

    refactor: Modifié conditions pour éviter les répétitions

--- src/mac-ip/ip.c
+++ src/mac-ip/ip.c
@@ -28,8 +28,9 @@ ERREUR_IP check_cidr(uint8_t cidr){
 }
 
 ERREUR_IP deinit(ip* ip){
-    if (check_pointeur_null(ip) == POINTEUR_NULL){
-        return POINTEUR_NULL;
+    ERREUR_IP err;
+    if ((err = check_pointeur_null(ip)) != NULLE){
+        return err;
     }
 
     free(ip);
@@ -39,8 +40,9 @@ ERREUR_IP deinit(ip* ip){
 }
 
 ERREUR_IP get_string(ip* ip, char* str){
-    if (check_pointeur_null(ip) == POINTEUR_NULL){
-        return POINTEUR_NULL;
+    ERREUR_IP err;
+    if ((err = check_pointeur_null(ip)) != NULLE){
+        return err;
     }
 
     sprintf(str, "%d.%d.%d.%d/%d", ip->adresse[0], ip->adresse[1], ip->adresse[2], ip->adresse[3], ip->cidr);
@@ -49,8 +51,9 @@ ERREUR_IP get_string(ip* ip, char* str){
 }
 
 ERREUR_IP set_adresse(ip* ip, uint8_t octet1, uint8_t octet2, uint8_t octet3, uint8_t octet4){
-    if (check_pointeur_null(ip) == POINTEUR_NULL){
-        return POINTEUR_NULL;
+    ERREUR_IP err;
+    if ((err = check_pointeur_null(ip)) != NULLE){
+        return err;
     }
 
     ip->adresse[0] = octet1;
@@ -62,12 +65,13 @@ ERREUR_IP set_adresse(ip* ip, uint8_t octet1, uint8_t octet2, uint8_t octet3, ui
 }
 
 ERREUR_IP set_octet_adresse(ip* ip, uint8_t octet, size_t index){
-    if (check_pointeur_null(ip) == POINTEUR_NULL){
-        return POINTEUR_NULL;
+    ERREUR_IP err;
+    if ((err = check_pointeur_null(ip)) != NULLE){
+        return err;
     }
 
-    if (check_index_octet(index) == INDEX_OCTET){
-        return INDEX_OCTET;
+    if ((err = check_index_octet(index)) != NULLE){
+        return err;
     }
 
     ip->adresse[index] = octet;
@@ -76,12 +80,13 @@ ERREUR_IP set_octet_adresse(ip* ip, uint8_t octet, size_t index){
 }
 
 ERREUR_IP set_cidr(ip* ip , uint8_t cidr){
-    if (check_pointeur_null(ip) == POINTEUR_NULL){
-        return POINTEUR_NULL;
+    ERREUR_IP err;
+    if ((err = check_pointeur_null(ip)) != NULLE){
+        return err;
     }
 
-    if (check_cidr(cidr) == VALEUR_CIDR){
-        return VALEUR_CIDR;
+    if ((err = check_cidr(cidr)) != NULLE){
+        return err;
     }
 
     ip->cidr = cidr;
