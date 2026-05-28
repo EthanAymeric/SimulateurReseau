34eb1fd14f5ca441c27f7423815c747361abefa7
Ethan <ethan.soobrayen@gmail.com>
2026-05-28 10:16:55 +0200

    feat : ajout de fonctions (incomplètes pour certaines) pour mieux utiliser les stations et pouvoir les relier à d'autres stations ou d'autres switch ; autres petites corrections syntaxiques

--- src/station/station.h
+++ src/station/station.h
@@ -9,7 +9,7 @@ typedef enum ERREUR_STATION {
     POINTEUR_NULL,
     VALEUR,
     FORMAT_STRING,
-    NULLE
+    OK
 }   ERREUR_STATION;
 
 typedef struct station station;
@@ -19,6 +19,8 @@ ERREUR_STATION deinit(station* station);
 
 ERREUR_STATION get_string(station* station, char* str);
 ERREUR_STATION set_station(station* station, ip ip, mac mac);
-ERREUR_STATION station_add_relation(station* st1, station* st2)
+ERREUR_STATION station_ajoute_relation_station(station* st1, station* st2);
+ERREUR_STATION station_suppr_relation(station* st1, station* st2);
+ERREUR_STATION station_ajoute_relation_switch(station* st, switch* sw);
 
 #endif
