762e201e7b79f4027c966762b02fd5eb9754c8b5
Eliott Wanner <eliott.wanner@etu.unistra.fr>
2026-06-07 15:29:24 +0200

    feat: Ajouté fonctions init, deinit, set_station et set_switch

--- src/appareil/appareil.c
+++ src/appareil/appareil.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 #include "appareil.h"
 #include "../switch/switch.h"
 #include "../station/station.h"
@@ -10,3 +12,77 @@ struct appareil {
         Switch* sw;
     } appareil;
 };
+
+ERREUR_CODE appareil_check_pointeur_null(void* ptr){
+    return ptr == NULL ? POINTEUR_NULL : OK;
+}
+
+appareil* appareil_init(){
+    appareil* ap = NULL;
+
+    ap = malloc(sizeof(appareil));
+    ap->type = INDEFINI;
+
+    return ap;
+}
+
+ERREUR_CODE appareil_deinit(appareil *appareil){
+    ERREUR_CODE err;
+    if ((err = appareil_check_pointeur_null(appareil)) != OK){
+        return err;
+    }
+
+    if (appareil->type == SWITCH){
+        switch_deinit(appareil->appareil.sw);
+        appareil->appareil.sw = NULL;
+    }
+    else if (appareil->type == STATION){
+        station_deinit(appareil->appareil.st);
+        appareil->appareil.st = NULL;
+    }
+
+    free(appareil);
+    appareil = NULL;
+
+    return OK;
+}
+
+ERREUR_CODE appareil_set_station(appareil *appareil, station *station){
+    ERREUR_CODE err; 
+    if ((err = appareil_check_pointeur_null(appareil)) != OK ||
+        (err = appareil_check_pointeur_null(station)) != OK){
+        return err;
+    }
+
+    if (appareil->type == SWITCH){
+        switch_deinit(appareil->appareil.sw);
+    }
+    else if (appareil->type == STATION){
+        station_deinit(appareil->appareil.st);
+    }
+
+    appareil->type = STATION;
+    appareil->appareil.st = station;
+
+    return OK;
+}
+
+ERREUR_CODE appareil_set_switch(appareil *appareil, Switch *sw){
+    ERREUR_CODE err; 
+    if ((err = appareil_check_pointeur_null(appareil)) != OK ||
+        (err = appareil_check_pointeur_null(sw)) != OK){
+        return err;
+    }
+
+    if (appareil->type == SWITCH){
+        free(appareil->appareil.sw);
+    }
+    else if (appareil->type == STATION){
+        free(appareil->appareil.st);
+    }
+
+    appareil->type = SWITCH;
+    appareil->appareil.sw = sw;
+
+    return OK;
+}
--- src/appareil/appareil.h
+++ src/appareil/appareil.h
@@ -1,11 +1,26 @@
 #ifndef APPAREIL_H
 #define APPAREIL_H
 
+#include "../error/error.h"
+#include "../station/station.h"
+#include "../switch/switch.h"
+#include <error.h>
+
 typedef struct appareil appareil;
 
 typedef enum TYPE_APPAREIL {
     SWITCH,
-    STATION
+    STATION,
+    INDEFINI
 } TYPE_APPAREIL;
+
+appareil* appareil_init();
+ERREUR_CODE appareil_deinit(appareil* appareil);
+
+ERREUR_CODE appareil_set_station(appareil* appareil, station* station);
+ERREUR_CODE appareil_set_switch(appareil* appareil, Switch* sw);
+ERREUR_CODE appareil_get_type(appareil* appareil, TYPE_APPAREIL* type);
+ERREUR_CODE appareil_get_station(appareil* appareil, station* station);
+ERREUR_CODE appareil_get_switch(appareil* appareil, Switch* sw);
 
 #endif
