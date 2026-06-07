684fe0af822fee6259aa861df74d6c5b62c6da83
NIEDERLAENDER GUILLAUME <guillaume.niederlaender@etu.unistra.fr>
2026-06-07 22:58:10 +0200

    feat: Finalisation du parser, ajout des fichiers configs

--- src/main.c
+++ src/main.c
@@ -1,19 +1,14 @@
 #include "mac-ip/mac.h"
 #include "switch/switch.h"
+#include "parser/parser.h"
 
 #include <stdlib.h>
 #include <stdio.h>
 
 int main() {
-    mac* mac = mac_init();
-    mac_set_uint64(mac, 255766639995918);
-    Switch* s = switch_init_with_parameter(mac, 8, 4096);
-    switch_set_commutation_table(s, 5, mac);
-    char* str = malloc(sizeof(char) * 255);
-    switch_show_commutation_table(s,str);
-    printf("%s\n", str);
-
-    mac_deinit(mac);
-    switch_deinit(s);
-    free(str);
+    FILE* fptr = file_init("config_files/config1.txt");
+    file_lan_create(fptr);
+    file_equipements_create(fptr, 3);
+    file_connexions_create(fptr, 2);
+    file_deinit(fptr);
 }
